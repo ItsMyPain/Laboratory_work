2f8345780a31336fbf24942a4844c9b91384314f
Pain <agibalov.sd@phystech.edu>
2021-03-23 16:39:54 +0300

    fixed 4th point and added 5th

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 int main() {
-    ofstream fout("5.txt");
+    ofstream fout("6.txt");
     list<int> a;
     //a.push_back(1);
     for (int i = 0; i < 100000; i++) {
         auto begin = chrono::high_resolution_clock::now();
         for (int j = 0; j < 1000; j++)
-            a.push_back(1);
+            a.push_front(1);
         auto end = chrono::high_resolution_clock::now();
         //if (i % 100 == 0)
         fout << chrono::duration_cast<chrono::nanoseconds>(end - begin).count() / 1000 << " " << a.size() << endl;
